c67c4d2ce2fa2b0bb282d89f8276702e9723c3a0
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-05-07 15:27:05 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,9 +1,9 @@
 module SongsHelper
-  def display_artist
-    if self.artist
-      artist_path(self.artist)
+  def display_artist(song)
+    if song.artist
+      link_to song.artist.name, artist_path(song.artist)
     else
-      edit_song_path(self)
+      link_to "Add Artist" edit_song_path(song)
     end
   end
 end
